3c35e5ea017da6cf8902cdee5cda46cbaae7607b
sidhys1 <sidhys1@github.com>
2020-10-20 11:43:09 -0400

    audit log

---
+++ messageUpdate.js
@@ -0,0 +1,12 @@
+const {MessageEmbed} = require('discord.js')
+module.exports=async(oldMessage,newMessage)=>{
+    let embed = new MessageEmbed()
+    .setTitle('Message Edited')
+    .setColor('BLUE')
+    .setDescription(`Message by <@${message.author.id}> edited in <#${oldMessage.channel.id}>`)
+    .addField('Before',oldMessage.content,true)
+    .addField('After',newMessage.content,true)
+    let channel = message.guild.channels.cache.find(ch=> ch.id === "714822326016933900")
+    if(!channel) return;
+    channel.send(deleteembed)
+}
