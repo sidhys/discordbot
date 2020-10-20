f71ee2a3b83046c3092232126b6d4d016437b541
sidhys1 <sidhys1@github.com>
2020-10-20 11:53:39 -0400

    Update messageUpdate.js

--- messageUpdate.js
+++ messageUpdate.js
@@ -5,8 +5,8 @@ module.exports=async(oldMessage,newMessage)=>{
     .setColor('BLUE')
     .setDescription(`Message by <@${oldMessage.author.id}> edited in <#${oldMessage.channel.id}>`)
     .addField('Before',oldMessage.content,true)
-    .addField('After',newMessage.content,true)
+    .addField('After', newMessage.content,true)
     let channel = oldMessage.guild.channels.cache.find(ch=> ch.id === "714822326016933900")
     if(!channel) return;
-    channel.send(deleteembed)
+    channel.send(embed)
 }
