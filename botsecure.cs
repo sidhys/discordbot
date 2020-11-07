dacb00611b72c73d4254812e92fc432e49a686a3
Sid <62297115+sidhys1@users.noreply.github.com>
2020-11-07 12:59:02 -0500

    Create botsecure.cs

---
+++ botsecure.cs
@@ -0,0 +1,55 @@
+        using System;
+        using System.Collections.Generic;
+        using System.Globalization;
+        using System.Linq;
+        using System.Text;
+        using System.Threading.Tasks;
+
+        namespace botsecure
+        {
+            class Program
+            {
+                static void Main(string[] args)
+                {         
+                
+                   private async void Main()
+                   {
+                   
+                  client = new DiscordSocketClient();
+                  commands = new CommandService();
+
+                  client.Log += Log;
+
+                 services = new ServiceCollection().BuildServiceProvider();
+
+                 await InstallCommands();
+
+                   }
+                   public CommandService commands;
+                   public DiscordSocketClient client;
+                   public IServiceProvider services;
+                  public char prefix = PREFIX.js;
+                
+                    Random rand = new Random();
+                    int randint= rand.Next(1, 10);
+                    int tries = 0;
+           
+                    while (rant != _GLOBAL.STARTER)
+                    {
+                        if (!int.TryParse(randint, out Guess))
+                        {
+                        FireServer()
+                        }
+
+                        Guess = Int32.Parse(input);
+
+                        if (randint != _GLOBAL.ENV)
+                        { 
+                        await 
+                        return status;
+                        }
+                    }
+                      
+                   }       
+                }
+            }
