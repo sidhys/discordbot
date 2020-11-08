586ff43fa2c63d9349c41e30ca4ac09ee98f22d7
Sid <62297115+sidhys1@users.noreply.github.com>
2020-11-07 23:27:54 -0500

    reaction verify is broken, added error handling so bot doesn't crash

--- index.js
+++ index.js
@@ -2601,8 +2601,13 @@ client.on('message', async (msg) => {
 	if(reaction.message.guild) return;		
 	if(reaction.message.channel.id === '714819052639486082') {
 	if(reaction.emoji.name === "✅") {
+		console.log('requested')
 		await reaction.message.guild.members.cache.get(user.id).roles.add('714833474166587425');
 		await reaction.message.guild.members.cache.get(user.id).roles.remove('714833374010933361');
+		
+		if(error) {
+		console.log(error)
+		}
 	}
  
 		}
