3745d13c40b048a4cd10604515d5092d0483aaac
Sid <62297115+sidhys1@users.noreply.github.com>
2021-01-12 12:46:29 -0500

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -11,8 +11,7 @@ module.exports = {
         await console.log('putting this here bc bot needs something to await to :troll:');
 		if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
         const commandName = args[0].toLowerCase();
-        const command = message.client.commands.get(commandName)
-	    || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+        const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
         if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`); 
         delete require.cache[require.resolve(`./${command.name}.js`)];
         try {
