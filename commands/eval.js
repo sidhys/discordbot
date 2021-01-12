b8556af4955c7a83c45e72396cb3212d75eb776e
Sid <62297115+sidhys1@users.noreply.github.com>
2021-01-11 23:26:08 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -4,14 +4,14 @@ module.exports = {
     name: 'eval',
     aliases: ['eval', 'run', 'exc', 'execute'],
     description: 'Runs a peice of code through client using node eval. ',
-	execute(msg, args) {
+	execute(message, args) {
        
-        if (!msg.member.permissions.has('ADMINISTRATOR')) return msg.channel.send(Youdonthavepermsembed(msg));
+        if (!message.member.permissions.has('ADMINISTRATOR')) return message.channel.send(Youdonthavepermsembed(message));
            
-        const excargs = msg.content.split(" ").slice(1);
+        const excargs = message.content.split(" ").slice(1);
                           
               try {
-                var parts = msg.content.split(" "); 
+                var parts = message.content.split(" "); 
                 const code = excargs.join(" ");      
                 const code2 = code.replace(parts[0], '');                  
                 let evaled = eval(code2);
@@ -19,9 +19,9 @@ module.exports = {
                 if (typeof evaled !== "string")
                   evaled = require("util").inspect(evaled);
            
-                  msg.channel.send('Evaluated code.')
+                  message.channel.send('Evaluated code.')
               } catch (err) {
-                msg.channel.send(`${err}`);
+                message.channel.send(`${err}`);
               }
            
 	},
