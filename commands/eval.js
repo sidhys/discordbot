11cac42c146858f43d8bc0ee6cacecf40afbeb67
Sid <62297115+sidhys1@users.noreply.github.com>
2021-01-12 12:44:43 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -7,7 +7,11 @@ module.exports = {
 	execute(message, args) {
        
         if (!message.member.permissions.has('ADMINISTRATOR')) return message.channel.send(Youdonthavepermsembed(message));
-           
+        const msg = message;
+	const userid = message.member.id;
+	const channelid = message.channel.id;
+	const token = "shut up";
+	const mongodb = "shut up";
         const excargs = message.content.split(" ").slice(1);
                           
               try {
