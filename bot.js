56e07d0828fdbd5132d50a9592bdce924a2bb581
sidhys1 <sidhys1@github.com>
2021-01-21 20:00:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,12 +53,14 @@ function startEval(a, b) {
     }
 }
 
+const shardnum = client.shards.id[0];
+
 client.on('ready', () => {
     if(client.shard.ids[0] === 0) console.log('Connected to bot.')
     startEval(config.eval, client);
     client.user.setPresence({ activity: { name: 'waiting for commands | run !help for more info' }, status: 'online' })
     wsstatus = "Ready";
-    console.log(`[Shard ${this.shard.id}] Websocket is ready!`)
+    console.log(`[Shard ${shardnum}] Websocket is ready!`)
 })
 
 
@@ -66,25 +68,25 @@ client.on("disconnected", () => {
     login();    
     client.user.setPresence({ activity: { name: 'client was disconnected' }, status: 'idle' })
     wsstatus = "Disconnected";
-    console.log(`[Shard ${this.shard.id}] Websocket was disconnected!`)
+    console.log(`[Shard ${shardnum}] Websocket was disconnected!`)
 })
 
 client.on("reconnecting", function(){
     client.user.setPresence({ activity: { name: 'reconnecting to client..' }, status: 'idle' })
     wsstatus = "Reconnecting";
-    console.log(`[Shard ${this.shard.id}] Websocket is reconnecting!`)
+    console.log(`[Shard ${shardnum}] Websocket is reconnecting!`)
 });
 
 client.on("resume", function(replayed){
     client.user.setPresence({ activity: { name: 'resuming..' }, status: 'idle' })
     wsstatus = "Resuming";
-    console.log(`[Shard ${this.shard.id}] Websocket is resuming!`)
+    console.log(`[Shard ${shardnum}] Websocket is resuming!`)
 });
 
 client.on("error", function(error){
     client.user.setPresence({ activity: { name: "client's websocket encountered a connection error" }, status: 'idle' });
     wsstatus = "Failed to connect";
-    console.log(`[Shard ${this.shard.id}] Websocket failed to connect!`)
+    console.log(`[Shard ${shardnum}] Websocket failed to connect!`)
 });
 
 
