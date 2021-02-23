60f9269e7bfdf1f5977ea190378dda9c449e905c
Sid <62297115+sidhys1@users.noreply.github.com>
2021-02-23 15:12:09 -0500

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -27,7 +27,6 @@ module.exports = {
         .setTitle("Bot")
         .setColor('BLUE')
         .setDescription(`Successfully changed ${nickuser}'s nickname to \`${nick}\`, ${msg.author}.`)
-        .setFooter(`${nick} is a pretty cringe nickname ngl`)
         .setTimestamp();
         msg.channel.send(nickembed) 
 	},
